851db073d8cff2ac82dde6b8051d000ea92cb4e7
Vanza Lutonda Maxime <maximevanza@live.fr>
2018-01-17 15:19:32 +0100

    Ma zouz elle est al

---
+++ test/unit/UtilTest.js
@@ -0,0 +1,70 @@
+describe("Factorial", function()
+{
+    var testCases = [
+        {
+            n: 0,
+            factorial: 1
+        },
+        {
+            n: 1,
+            factorial: 1
+        },
+        {
+            n: 2,
+            factorial: 2
+        },
+        {
+            n: 3,
+            factorial: 6
+        },
+        {
+            n: 4,
+            factorial: 24
+        },
+    ];
+
+    testCases.forEach(function (testCase) {
+        it("should returns " + testCase.factorial + " when n = " + testCase.n, function () {
+            var result = Util.factorial(testCase.n);
+            expect(result).toEqual(testCase.factorial);
+        })
+    });
+})
+
+describe("Arrangement", function()
+{
+    var testCases = [
+        {
+            n: 0,
+            r: 0,
+            arrangement: 1
+        },
+        {
+            n: 1,
+            r: 0,
+            arangement: 1
+        },
+        {
+            n: 2,
+            r: 1,
+            arrangement: 2
+        },
+        {
+            n: 4,
+            r: 2,
+            factorial: 12
+        },
+        {
+            n: 4,
+            r: 3,
+            factorial: 24
+        },
+    ];
+
+    testCases.forEach(function (testCase) {
+        it("should returns " + testCase.factorial + " when n = " + testCase.n, function () {
+            var result = Util.factorial(testCase.n);
+            expect(result).toEqual(testCase.factorial);
+        })
+    });
+})
